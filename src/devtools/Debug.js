95ab6c40325f8f63c987813ac4c26d6b080a3780
gozala <rfobic@gmail.com>
2016-05-06 16:24:25 -0700

    Update Debug.js

--- src/devtools/Debug.js
+++ src/devtools/Debug.js
@@ -8,29 +8,20 @@ import type {Never} from "reflex"
 export type Command <input, state> =
   | { type: "send", send: input }
   | { type: "reset", reset: state }
-
-export type Action <input, state> =
-  { type: "#debug", debug: Command<input, state> }
 */
 
 export const send = /*::<input, state>*/
-  (message/*:input*/)/*:Task<Never, Action<input, state>>*/ =>
+  (message/*:input*/)/*:Task<Never, Command<input, state>>*/ =>
   Task.succeed
-  ( { type: "#debug"
-    , debug:
-      { type: "send"
-      , send: message
-      }
+  ( { type: "send"
+    , send: message
     }
   )
 
 export const reset = /*::<input, state>*/
-  (state/*:state*/)/*:Task<Never, Action<input, state>>*/ =>
+  (state/*:state*/)/*:Task<Never, Command<input, state>>*/ =>
   Task.succeed
-  ( { type: "#debug"
-    , debug:
-      { type: "reset"
-      , reset: state
-      }
+  ( { type: "reset"
+    , reset: state
     }
   )
