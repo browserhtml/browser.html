9b023c56327c63cb33c29f917251a494155f6725
Mathieu Agopian <mathieu@agopian.info>
2017-02-23 11:49:23 +0100

    Fixes #1242: second batch of reformated files to follow standard

--- src/Devtools/Log.js
+++ src/Devtools/Log.js
@@ -1,16 +1,11 @@
 /* @flow */
 
-import {Effects, Task, html, forward} from "reflex"
-import {merge, always, nofx} from "../Common/Prelude"
-import {ok, error} from "../Common/Result"
-import * as Runtime from "../Common/Runtime"
-import * as Unknown from "../Common/Unknown"
+import {Effects, html} from 'reflex'
+import {nofx} from '../Common/Prelude'
+import * as Runtime from '../Common/Runtime'
+import * as Unknown from '../Common/Unknown'
 
-
-import type {Address, Never, DOM, Init, Update, View, AdvancedConfiguration} from "reflex"
-import type {Result} from "../Common/Result"
-import type {URI, ID} from "../Common/Prelude"
-
+import type {Address, DOM} from 'reflex'
 
 export type Model <model, action> =
   { mode: 'raw' | 'json' | 'none'
@@ -21,34 +16,29 @@ export type Action <model, action> =
   | { type: "Debuggee", debuggee: action }
 
 type Step <model, action> =
-  [ Model<model, action>
-  , Effects<Action<model, action>>
+  [ Model<model, action>,
+   Effects<Action<model, action>>
   ]
-
-
-
-
-const NoOp = always({ type: "NoOp" });
 
 export const init = <model, action, flags>
   ():Step<model, action> =>
-  ( [ { mode:
-        ( Runtime.env.log === 'json'
+  ([ { mode:
+        (Runtime.env.log === 'json'
         ? 'json'
         : Runtime.env.log != null
         ? 'raw'
         : 'none'
         )
-      }
-    , Effects.none
+      },
+     Effects.none
     ]
   )
 
 export const update = <model, action>
-  ( model:Model<model, action>
-  , action:Action<model, action>
+  (model:Model<model, action>,
+   action:Action<model, action>
   ):Step<model, action> =>
-  ( action.type === "NoOp"
+  (action.type === 'NoOp'
   ? nofx(model)
   : action.type === 'Debuggee'
   ? log(model, action.debuggee)
@@ -56,21 +46,21 @@ export const update = <model, action>
   )
 
 const log = <model, action>
-  ( model:Model<model, action>
-  , action:action
+  (model:Model<model, action>,
+   action:action
   ):Step<model, action> => {
-    ( model.mode === 'raw'
+  (model.mode === 'raw'
     ? console.log('Action >>', action)
     : model.mode === 'json'
     ? console.log(`Action >> ${JSON.stringify(action)}`)
     : null
-    );
+    )
 
-    return nofx(model)
-  }
+  return nofx(model)
+}
 
 export const view = <model, action>
-  ( model:Model<model, action>
-  , address:Address<Action<model, action>>
+  (model:Model<model, action>,
+   address:Address<Action<model, action>>
   ):DOM =>
   html.noscript()
